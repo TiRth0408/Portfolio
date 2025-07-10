94b0fc79dceff0163f469aae54a88f7ddd9b6972
Kadivar Tirthkumar <128504479+TiRth0408@users.noreply.github.com>
2025-07-10 13:22:00 +0530

    Your message

--- script.js
+++ script.js
@@ -54,6 +54,26 @@ gsap.from(".education-card", {
     stagger: 0.2
 });
 
+
+document.getElementById('downloadPdfBtn').addEventListener('click', function (e) {
+    e.preventDefault();
+    var cvContent = document.getElementById('cvContent');
+    cvContent.classList.remove('d-none');
+    setTimeout(function() {
+        html2pdf()
+            .set({
+                margin: 0.5,
+                filename: 'Tirth Resume.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+            })
+            .from(cvContent)
+            .save();
+    }, 0);
+});
+
+
 // Show CV Button Toggle
 const showCvBtn = document.getElementById("showCvBtn");
 const cvContent = document.getElementById("cvContent");
